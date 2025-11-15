898cd76c67613dba1463829df935f48afd807e1e
Movin Vinusandha <movinvinusandha@gmail.com>
2025-11-15 12:03:19 +0530

    Build a layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Grid, GridItem } from '@chakra-ui/react'
 
 function App() {
-  return <Button colorScheme='blue'>Button</Button>
+  return <Grid templateAreas={{
+    base: `"nav" "main" "footer"`
+  }}>
+    <GridItem area="nav" bg='gold' >Nav</GridItem>
+    <GridItem area="main" bg='blue.300' >Main</GridItem>
+    <GridItem area="footer" bg='green.300' >Footer</GridItem>
+  </Grid>
 }
 
 export default App
