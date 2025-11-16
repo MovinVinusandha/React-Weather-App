58f5833aaceb5ec013880e15f235787e75d026d6
Movin Vinusandha <movinvinusandha@gmail.com>
2025-11-16 10:23:56 +0530

    Create a custom hook to fetch the weather

--- src/components/CurrentWeather.tsx
+++ src/components/CurrentWeather.tsx
@@ -1,34 +1,13 @@
-import { useEffect, useState } from "react"
-import apiClient from "../services/api-client"
 import { Text } from "@chakra-ui/react"
-
-interface Current {
-  temp_c: number
-}
-
-interface Location {
-  name: string
-}
-
-interface FeatchWeatherResponse {
-  location: Location,
-  current: Current
-}
+import useWeather from "../hooks/useWeather";
 
 interface Props {
   location: string
 }
 
 function CurrentWeather({ location }: Props) {
-  const [weather, setWeather] = useState<FeatchWeatherResponse>()
-  const [error, setError] = useState('')
+  const { weather, error } = useWeather(location);
 
-  useEffect(() => {
-    apiClient.get<FeatchWeatherResponse>(`/current.json?q=${location}`)
-    .then(res => setWeather(res.data))
-    .catch(err => setError(err.message))
-  }, [location])
-  
   return (
     <>
       {error && <Text>{error}</Text>}
---
+++ src/hooks/useWeather.ts
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+interface Current {
+  temp_c: number
+}
+
+interface Location {
+  name: string
+}
+
+interface FeatchWeatherResponse {
+  location: Location,
+  current: Current
+}
+
+const useWeather = ( location: string ) => {
+  const [weather, setWeather] = useState<FeatchWeatherResponse>();
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    apiClient
+      .get<FeatchWeatherResponse>(`/current.json?q=${location}`, { signal: controller.signal })
+      .then((res) => setWeather(res.data))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+    });
+
+      return () => controller.abort();
+  }, [location]);
+
+  return { weather, error };
+};
+
+export default useWeather;
